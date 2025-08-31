c7a939b02b9b90cc2313fbe99d157479ba1323e9
Sigifredo Chacón Balladares <115943275+SigifredoChacon@users.noreply.github.com>
2025-08-30 21:54:21 -0600

    Se arreglaron errores

--- models/postgresql/userModel.js
+++ models/postgresql/userModel.js
@@ -47,7 +47,7 @@ export class userModel {
                 u."Contrasena",
                 u."CedulaCarnet",
                 u."Estado",
-                r."nombre" AS "RolNombre"
+                r."Nombre" AS "RolNombre"
              FROM "usuario" u
              JOIN "rol" r ON u."idRol" = r."idRol"
              WHERE u."CorreoEmail" = $1 OR u."CorreoInstitucional" = $2;`,
